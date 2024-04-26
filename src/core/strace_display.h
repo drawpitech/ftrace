0930ce504b01b53edc03222fdfd5533aa189d437
Clément <clement2104.boillot@gmail.com>
2024-04-26 11:46:15 +0200

    feat: add function to parse process maps

--- src/core/strace_display.h
+++ src/core/strace_display.h
@@ -6,6 +6,21 @@
 */
 
 #pragma once
+
+#include <stddef.h>
+#include <linux/limits.h>
 #include "context.h"
+
+typedef struct {
+    char m_path[PATH_MAX];
+    ptrdiff_t m_start_addr;
+    ptrdiff_t m_end_addr;
+} map_item_t;
+
+typedef struct {
+    map_item_t *m_items;
+    size_t m_len;
+    size_t m_capacity;
+} maps_t;
 
 void display_syscall(context_t *ctx);
